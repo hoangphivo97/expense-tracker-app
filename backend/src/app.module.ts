51be8d9e6cbe11009b3f1d85c17b4a1f8c9587f0
hoangphivo97 <phivo1397@gmail.com>
2025-03-19 19:08:23 +0700

    update env file, security

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -3,10 +3,13 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserModule } from './login/user.module';
 import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [UserModule,
+  imports: [
     MongooseModule.forRoot('mongodb://localhost:27017/expense-tracker-app'),
+    ConfigModule.forRoot({isGlobal: true}),
+    UserModule,
   ],
   controllers: [AppController],
   providers: [AppService],
--- backend/src/login/user.module.ts
+++ backend/src/login/user.module.ts
@@ -4,15 +4,20 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { UserSchema } from 'src/schema/user.schema';
 import { UserController } from './user.controller';
 import { JwtModule } from '@nestjs/jwt';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
   providers: [UserService],
   exports: [UserModule, JwtModule],
   imports: [MongooseModule.forFeature([{ name: 'User', schema: UserSchema }]),
-  JwtModule.register({
-    secret: "a004976547ab3937176c0f918b680f4d18e7d74e6cebccfa9fa452bda6b723fa",
-    signOptions: { expiresIn: '1h' }
-  })
+  JwtModule.registerAsync({
+    imports: [ConfigModule],
+    inject: [ConfigService],
+    useFactory: (configService: ConfigService) => ({
+      secret: configService.get<string>('JWT_SECRET'),
+      signOptions: { expiresIn: '1h' },
+    }),
+  }),
   ],
   controllers: [UserController]
 })
