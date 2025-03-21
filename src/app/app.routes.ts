be6180cb98d63e1464e55d092698b1308cc9310b
hoangphivo97 <phivo1397@gmail.com>
2025-03-21 15:15:34 +0700

    update lazy loading

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,13 +1,11 @@
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './features/expenses/login/login.component';
-import { ExpenseListComponent } from './features/expenses/expense-list/expense-list.component';
-import { authGuard } from './services/RouteGuard/auth.guard';
 import { NgModule } from '@angular/core';
 
 export const routes: Routes = [
     { path: 'login', component: LoginComponent },
     { path: '', redirectTo: '/login', pathMatch: 'full' },
-    { path: 'expense-list', component: ExpenseListComponent, canActivate: [authGuard] }
+    { path: 'expense-list', loadComponent: () => import("./features/expenses/expense-list/expense-list.component").then(m => m.ExpenseListComponent) }
 ];
 
 @NgModule({
